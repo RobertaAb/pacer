045dfc302e5a8e149744a1c5e9ca0e75648a5657
Roberta Abreu <robertaabreu77@gmail.com>
2022-06-03 22:43:44 +0000

    alteração pow

--- src/main/java/br/gov/fatec/pacer/controller/RespostaController.java
+++ src/main/java/br/gov/fatec/pacer/controller/RespostaController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,12 +28,12 @@ public class RespostaController {
 
 	@GetMapping("/listar")
 	public List<Resposta> ListarTodos() {
-		return respostaService.findAll();
+		return respostasService.findAll();
 	}
 
 	@PostMapping("/salvarResposta")
 	public ResponseEntity<Object> saveResposta(@RequestBody @Valid RespostaDto respostaDto) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(respostaService.save(respostaDto));
+		return ResponseEntity.status(HttpStatus.CREATED).body(respostasService.save(respostaDto));
 	}
 
 	@DeleteMapping(path = { "/{id}" })
